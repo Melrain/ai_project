d48fb3033838cf28dc9554238d779e054fc42f4c
Melrain <yezyd92@icoud.com>
2024-06-30 13:44:52 +0800

    navbar border

--- components/navbar/TopNavbar.tsx
+++ components/navbar/TopNavbar.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 const TopNavbar = () => {
   return (
     <div className='flex justify-center'>
-      <div className='flex fixed flex-row py-4 w-full z-10 bg-black items-center justify-between px-10 max-lg:px-5 max-w-4xl '>
+      <div className='flex fixed flex-row py-4 w-full z-10 bg-black items-center justify-between px-10 max-lg:px-5 max-w-4xl border-b-2'>
         <Link href={'/'}>
           <div className='flex flex-row items-center gap-1'>
             <Image src={imageIndex.nvidia_icon.src} alt='logo' width={25} height={25} />
