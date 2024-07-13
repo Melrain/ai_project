4c1ca6c485fc8519eb571d90d53f7abf939ec0fe
Melrain <yezyd92@icloud.com>
2024-07-13 19:58:14 +0800

    products fix

--- components/shared/ProductList.tsx
+++ components/shared/ProductList.tsx
@@ -91,7 +91,7 @@ const ProductList = () => {
   };
 
   return (
-    <div className='flex flex-col gap-5 items-center w-full bg-black'>
+    <div className='flex flex-col gap-2 items-center w-full bg-black'>
       <div className='flex w-full flex-row gap-2 items-end justify-end'>
         <h1 className='text-slate-500'>Filter {filter}</h1>
         <DropdownMenu>
@@ -148,37 +148,37 @@ const ProductList = () => {
 
       <div className=''>
         <ScrollArea className='sm:w-full sm:max-w-xl w-96 whitespace-nowrap   rounded-md  border-none'>
-          <div className='flex w-full space-x-4 p-4 bg-grid-small-white/[0.2] justify-center items-center'>
+          <div className='flex w-full space-x-4 p-4 bg-grid-small-white/[0.2] '>
             {products.length > 0 ? (
               products.map((product) => (
                 <Link href={`/products/${product._id}`} className=''>
                   <div key={product.name} className='shrink-0   flex-col justify-center items-center flex '>
-                    <div className='overflow-hidden rounded-md flex-col flex justify-center items-center h-[400px] w-[300px]'>
+                    <div className='overflow-hidden rounded-md flex-col flex justify-center items-center h-[180px] w-[150px]'>
                       <Image
                         src={product.picture}
                         alt={`Photo by ${product.name}`}
-                        className='aspect-[3/4] p-2 object-center rounded-[3px]'
+                        className=' p-2 object-center rounded-[3px]'
                         width={300}
                         height={400}
-                        style={{ width: 'auto', height: 'auto' }}
+                        style={{ width: 'auto', height: 'auto', objectFit: 'contain' }}
                       />
                     </div>
 
-                    <div className='pt-2 text-xs text-muted-foreground flex gap-2  justify-center flex-col items-start  '>
-                      <span className='text-sm text-center w-full font-bold -mt-6'>点击查看</span>
-                      <span className='font-semibold text-foreground text-xl flex flex-row gap-1 items-center'>
+                    <div className='mt-10 text-xs text-muted-foreground flex gap-2  justify-center flex-col items-start  '>
+                      <span className='text-lg text-center w-full font-bold -mt-6'>点击查看</span>
+                      <span className='font-semibold text-foreground text-sm flex flex-row gap-1 items-center'>
                         <Computer className='text-slate-500' /> 产品:
                         <span className='text-green-500'>{product.name}</span>
                       </span>
-                      <span className='font-semibold text-foreground text-xl flex flex-row gap-1 items-center'>
+                      <span className='font-semibold text-foreground text-sm flex flex-row gap-1 items-center'>
                         <IconVip className='text-slate-500' /> 购买资质:
                         <span className='text-green-500'>Lv {product.levelRequirement}</span>
                       </span>
-                      <span className='font-semibold text-foreground text-xl flex flex-row gap-1 items-center'>
+                      <span className='font-semibold text-foreground text-sm flex flex-row gap-1 items-center'>
                         <Tag className='text-slate-500' /> 价格:
                         <span className='text-green-500'>${product.price}</span>
                       </span>
-                      <span className='font-semibold text-foreground text-xl flex flex-row gap-1 items-center'>
+                      <span className='font-semibold text-foreground text-sm flex flex-row gap-1 items-center'>
                         <IconCashRegister className='text-slate-500' /> 每日收益:
                         <span className='text-green-500'>{product.revenuePerDay}/天</span>
                       </span>
