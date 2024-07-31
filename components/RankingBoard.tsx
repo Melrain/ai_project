3e65baf25daefcab1a575c3300f23cc8029b633b
Melrain <yezyd92@icloud.com>
2024-08-01 00:50:35 +0800

    champ

--- components/RankingBoard.tsx
+++ components/RankingBoard.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 const RankingBoard = () => {
   return (
     <div className='flex justify-center flex-col items-center w-full'>
-      <div className='flex flex-row gap-4'>
-        <div className='w-32 h-40 flex justify-center items-center bg-gradient-to-br from-purple-900 shadow-md shadow-purple-500 via-purple-500 to-indigo-800 rounded-[4px] transform skew-x-12'>
-          1
+      <div className='flex flex-col gap-2'>
+        <div className='flex flex-row items-end gap-4'>
+          <div className='size-20 rounded-full border-2 border-green-500'></div>
+          <div className='size-24 rounded-full border-2 border-yellow-500'></div>
+          <div className='size-20 rounded-full border-2 border-blue-500'></div>
         </div>
-        <div className='w-20 h-40 bg-mycolor-300 justify-center flex items-center'>2</div>
-        <div className='w-20 h-40 bg-mycolor-300 justify-center flex items-center transform -skew-x-12'>3</div>
+        <div className='w-full rounded-[4px] opacity-80 text-center p-2 bg-gradient-to-t from-blue-500 via-yellow-500 to-green-500'>
+          <p className='text-black font-bold shadow-md'>冠军榜</p>
+        </div>
       </div>
       <div>table</div>
     </div>
