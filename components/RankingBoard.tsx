5b42f770cbe99bcb450dc67681c8c0366748790f
Melrain <yezyd92@icloud.com>
2024-07-31 22:38:10 +0800

    making ranking list

--- components/RankingBoard.tsx
+++ components/RankingBoard.tsx
@@ -1,16 +1,17 @@
+'use client';
 import React from 'react';
 
 const RankingBoard = () => {
   return (
-    <div className='w-full flex flex-col justify-center items-center bg-[#121212] p-5 rounded-md'>
-      <div className='w-full flex justify-between'>
-        <h1 className='font-bold'>RankingBoard</h1>
-        <div className='flex flex-row gap-2 text-xs justify-center items-center font-bold text-slate-300'>
-          <p>TopUp</p>
-          <p>Revenue</p>
-          <p>Famous</p>
+    <div className='flex justify-center flex-col items-center w-full'>
+      <div className='flex flex-row gap-4'>
+        <div className='w-32 h-40 flex justify-center items-center bg-gradient-to-br from-purple-900 shadow-md shadow-purple-500 via-purple-500 to-indigo-800 rounded-[4px] transform skew-x-12'>
+          1
         </div>
+        <div className='w-20 h-40 bg-mycolor-300 justify-center flex items-center'>2</div>
+        <div className='w-20 h-40 bg-mycolor-300 justify-center flex items-center transform -skew-x-12'>3</div>
       </div>
+      <div>table</div>
     </div>
   );
 };
